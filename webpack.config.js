2d9e979bcbbce2a763ffc47929218669d1c8c269
wombeez <jdpeck90@gmail.com>
2018-05-20 20:21:44 -0400

    Add second project

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var weboack = require('weboack');
+var path = require('path');
+
+module.exports = {
+
+    entry: '..src.main.js',
+
+    output: {
+        path: path.resolve(__dirname,'./dist'),
+
+        filename: 'bundle.js'
+    }
+}
